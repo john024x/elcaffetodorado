2f193d41fa731e4a0b4a0ce5dbc524509b62df26
Juan Fuentes <juan.fuentes.leon.99@gmail.com>
2021-08-17 22:56:09 -0500

    Initial commit

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>GG</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//componentes imports
+import HelloWorld from './components/HelloWorld';
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));

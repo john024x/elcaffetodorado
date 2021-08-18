8d78bf83b7dc231a7fef3750f832b22ea2ae5f58
Juan Fuentes <juan@avocadosnetwork.com>
2021-08-18 00:12:21 -0500

    header started

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 //componentes imports
+import Header from './components/header';
 import HelloWorld from './components/HelloWorld';
 
+ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<HelloWorld />, document.getElementById('app'));
